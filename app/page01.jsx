504e347ee187a5209a60b910468c09a8ca2d8e4f
akira0716 <akira.inaji.0716@gmail.com>
2024-01-13 15:44:12 +0900

    起動時画面の作成

--- app/page01.jsx
+++ app/page01.jsx
@@ -1,15 +1,75 @@
 import { router } from "expo-router";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View, Pressable, Image } from "react-native";
 
 export default function Page() {
-  const onClick = () => {
-    router.replace("/");
+  const [createBtnFlg, setCreateBtnFlg] = useState(true);
+  const [loginBtnFlg, setLoginBtnFlg] = useState(true);
+
+  const onPressBtnStyleChange = (mode, kind) => {
+    let state = true;
+
+    if (mode === 0) {
+      state = false;
+    }
+
+    if (kind === 0) {
+      setCreateBtnFlg(state);
+    } else {
+      setLoginBtnFlg(state);
+    }
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>ページ01</Text>
-      <Button title="戻る" onPress={onClick} />
+      <View style={styles.titleWrap}>
+        <Image
+          style={{
+            width: "90%",
+            height: "90%",
+            objectFit: "contain",
+          }}
+          source={require("../assets/ReactNative01.png")}
+        />
+        <View style={{ position: "absolute", top: 20, right: 50, zIndex: 1 }}>
+          <Image
+            style={{
+              width: 100,
+              height: 100,
+            }}
+            source={require("../assets/Rhaguruma.png")}
+          />
+        </View>
+      </View>
+      <View style={styles.imgWrap}>
+        <Image
+          style={{
+            width: "90%",
+            height: "90%",
+          }}
+          source={require("../assets/StartPageImg.png")}
+        />
+      </View>
+      <View>
+        <View style={styles.entryWrap1}>
+          <Pressable
+            onPressIn={() => onPressBtnStyleChange(0, 0)}
+            onPressOut={() => onPressBtnStyleChange(1, 0)}
+            style={createBtnFlg ? styles.button : styles.button2}
+          >
+            <Text style={styles.btnText}>New Account</Text>
+          </Pressable>
+        </View>
+        <View style={styles.entryWrap2}>
+          <Pressable
+            onPressIn={() => onPressBtnStyleChange(0, 1)}
+            onPressOut={() => onPressBtnStyleChange(1, 1)}
+            style={loginBtnFlg ? styles.button : styles.button2}
+          >
+            <Text style={styles.btnText}>Login</Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 }
@@ -17,12 +77,57 @@ export default function Page() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
+    height: "100%",
+  },
+  titleWrap: {
+    display: "flex",
     alignItems: "center",
     justifyContent: "center",
+    height: "45%",
+    position: "relative",
   },
-  title: {
-    margin: 20,
-    fontSize: 30,
-    fontWeight: "bold",
+  imgWrap: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "30%",
+  },
+  entryWrap1: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "25%",
+    paddingTop: 30,
+  },
+  entryWrap2: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "25%",
+    paddingBottom: 30,
+  },
+  button: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#50b9df",
+    width: 250,
+    height: 50,
+    borderRadius: 50,
+    elevation: 3,
+  },
+  button2: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#50b9df",
+    width: 250,
+    height: 50,
+    borderRadius: 50,
+    elevation: 1,
+    marginTop: 3,
+  },
+  btnText: {
+    fontSize: 20,
   },
 });
