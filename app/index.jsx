1133f2d997ce7aba89aa7ea2f563d3712938d20c
akira0716 <akira.inaji.0716@gmail.com>
2024-01-13 15:42:43 +0900

    supabase Insert記法

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,26 @@
 import "expo-router/entry";
 import { Button, StyleSheet, Text, View, Pressable } from "react-native";
 import { Link, router } from "expo-router";
+import {
+  fetchDataList,
+  addDataItem,
+  deleteDataItem,
+} from "../utils/supabaseFunction";
 
 const Home = () => {
-  const onClick = () => {
+  const onPressAdd = async () => {
+    // idは自動採番のため、指定しない。
+    const data = {
+      classification: "食費",
+      amount: 1000,
+      date: "2024-01-11",
+      extra_area: null,
+    };
+
+    await addDataItem(data);
+  };
+
+  const onPress = () => {
     router.replace("/page01");
   };
 
@@ -18,11 +35,11 @@ const Home = () => {
       </View>
       <View style={styles.wrap}>
         <Text>Buttonコンポーネント</Text>
-        <Button title="Go!!!" onPress={onClick} />
+        <Button title="Go!!!" onPress={onPress} />
       </View>
       <View style={styles.wrap}>
         <Text>Pressableコンポーネント</Text>
-        <Pressable style={styles.button} onPress={onClick}>
+        <Pressable style={styles.button} onPress={onPressAdd}>
           <Text style={styles.text}>GO!!!</Text>
         </Pressable>
       </View>
